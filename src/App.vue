c14130ad513c17fcf84abe6a531f49f21cf3d839
Remy Pearlstone <remypearlstone@Remys-MacBook-Pro.local>
2020-05-19 22:10:38 -0600

    remoced unnecessry files

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,64 @@
 <template>
   <div id="app">
-    <CardComparison />
+    <p>Card type: {{currentResult['card_type']}}</p>
+    <p>Credit rating: {{currentResult['credit_rating']}}</p>
+    <TwoCards
+        :cardOneData="currentResult['card1']"
+        :cardTwoData="currentResult['card2']"
+      />
   </div>
 </template>
-
 <script>
 
-import CardComparison from './components/CardComparison.vue'
+import axios from "axios";
+import TwoCards from "./components/TwoCards";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    CardComparison,
+    TwoCards
+  },
+  created() {
+    this.getCardData();
+  },
+  methods: {
+    getCardData() {
+      axios.get("https://lucianet.s3.amazonaws.com/fool_cc_compare.json").then(res => (this.tempFullList = res.data))
+      .then(() => {
+        // fake search for now, just add random item to 'current result'
+        this.currentResult = this.tempFullList[Math.floor(Math.random()*this.tempFullList.length)];
+        // check for null cards and add placeholder data where missing
+        // add other missing fields such as a placeholder image
+        // etc
+        if (!this.currentResult.card1) {
+          this.currentResult.card1 = {
+            offer_name: 'Missing card data'
+          }
+        }
+        if (!this.currentResult.card2) {
+          this.currentResult.card2 = {
+            offer_name: 'Missing card data'
+          }
+        }
+        console.log(this.currentResult)
+      })
+    }
+  },
+  data: () => {
+    return {
+      currentResult: {
+        card1: {
+          offer_name: ''
+        },
+        card2: {
+          offer_name: ''
+        },
+      },
+      tempFullList: []
+    };
   }
-}
+};
 </script>
-
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
---
+++ src/components/TwoCards.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="card">
+    <div class="two-card">
+      <div class="card-name">
+        <div class="card-title">{{cardOneData.offer_name}}</div>
+        <div class="card-image">
+          <img :src="cardOneData.offer_image" />
+        </div>
+        <button class="card-apply">Apply Now</button>
+        <div class="fees">{{cardOneData.annual_fee}}</div>
+        <div class="balance-fee">{{cardOneData.balance_transfer_fee}}</div>
+      </div>
+      <div class="card-name">
+        <div class="card-title">{{cardTwoData.offer_name}}</div>
+        <div class="card-image">
+          <img :src="cardTwoData.offer_image" />
+        </div>
+        <button class="card-apply">Apply Now</button>
+        <div class="fees">{{cardTwoData.annual_fee}}</div>
+        <div class="balance-fee">{{cardTwoData.balance_transfer_fee}}</div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Card",
+  props: ["cardOneData", "cardTwoData"],
+  data: () => {
+    return {};
+  }
+};
+</script>
+
+<style>
+.card {
+  border: 1px solid red;
+  display: flex;
+  flex-direction: column;
+}
+.credit-score {
+  width: 15em;
+}
+.card-button {
+  width: 15em;
+}
+.two-card {
+  display: flex;
+  justify-content: space-evenly;
+}
+.card-name {
+  padding: 1em 0;
+  width: 30em;
+  border: 1px solid greenyellow;
+}
+.card-title {
+  padding-bottom: 1em;
+}
+.card-apply {
+  background: green;
+  color: white;
+  font-size: 1em;
+  font-weight: bold;
+  width: 15em;
+  margin-top: 1em;
+  border-radius: 5px;
+  height: 3em;
+}
+img {
+  width: 15em;
+}
+</style>
